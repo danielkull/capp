fbd967adace93287cf79a2d2f9b4c071326a1799
danielkull <danielkull117@gmail.com>
2023-06-25 18:26:24 +0200

    fix: stefans view is visible and adds @ alias for path finding

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// Import of Aliaases @ for File path which we know from Vue CLI
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    // Here we define the respective alias
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
