71d67c8f7db975ca0ad30310a669d5133b8d0127
kiraGit67 <kirsten@sais.de>
2023-06-26 22:39:01 +0200

    feat: adds src/components/Calender.vue

---
+++ src/components/Calender.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Kalender</h1>
+  <h2>Übersicht über alle Fahrt-Termine</h2>
+  <div class="container">
+    <div class="calendar">
+      <div class="calendar-header">
+        <span class="month-picker" id="month-picker">Monat</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: var(--l-font);
+}
+
+h2 {
+  font-size: calc(var(--m-font) * 1.5);
+}
+</style>
