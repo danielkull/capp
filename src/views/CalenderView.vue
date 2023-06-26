77a91bae8b83a06948a06868b452ceda555ee139
kiraGit67 <kirsten@sais.de>
2023-06-26 22:39:49 +0200

    feat: adds src/views/CalenderView.vue

---
+++ src/views/CalenderView.vue
@@ -0,0 +1,14 @@
+<template>
+  <calender />
+</template>
+
+<script>
+import Calender from "@/components/Calender.vue";
+export default {
+  components: {
+    Calender,
+  },
+};
+</script>
+
+<style scoped></style>
