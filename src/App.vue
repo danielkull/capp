6dec5497894bf5f99e2e1c22215b5a81ed006987
kiraGit67 <kirsten@sais.de>
2023-06-26 22:41:04 +0200

    feat: adds new router-link to CalenderView.vue into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/stefansView">Stefans View</router-link>
+    <router-link to="/stefansView">Stefans View</router-link> |
+    <router-link to="/calendar">Kalender</router-link>
   </nav>
   <router-view />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
