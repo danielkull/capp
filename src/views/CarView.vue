4c1c531be7fb3f16f8a6b763db0f22698043a14e
kiraGit67 <kirsten@sais.de>
2023-07-06 17:54:15 +0200

    fix: displays cars correctly now by defining cars as null

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -3,40 +3,24 @@ import { supabase } from "@/lib/supabaseClient";
 export default {
   data() {
     return {
-      brands: [],
-      carTypes: [],
-      car: {},
+      carID: this.$route.params.id,
+      car: null,
     };
   },
   components: {},
   mounted() {
-    this.getBrands();
-    this.getCarTypes();
     this.getCar();
   },
   methods: {
-    async getBrands() {
-      const { data } = await supabase.from("brands").select();
-      this.brands = data;
-      console.log(this.brands);
-    },
-    async getCarTypes() {
-      const { data } = await supabase
-        .from("car_types")
-        .select(`id, car_type_name, category, brand_id, brands ( brand_name )`)
-        .order("brand_id", { ascending: true })
-        .order("id", { ascending: true })
-        .order("category", { ascending: true });
-      this.carTypes = data;
-      console.log(this.carTypes);
-    },
     async getCar() {
       const { data } = await supabase
         .from("cars")
         .select(
-          `id, type_id, user_id, kw, year_of_construction, max_speed, trunk_volume_in_liters, img_source, count_of_seats, users (id, username, firstname, lastname), car_types ( id, car_type_name, category, brand_id, brands ( id, brand_name ) ), cars_features ( id, car_id, feature_id, features ( id, feature_name ) )`
+          `*, users ( id, username, firstname, lastname ), 
+          car_types ( id, car_type_name, category, brand_id, brands ( id, brand_name ) ), 
+          cars_features ( id, car_id, feature_id, features ( id, feature_name ) )`
         )
-        .eq("id", this.$route.params.id);
+        .eq("id", this.carID);
       this.car = data[0];
       console.log(this.car);
     },
@@ -46,40 +30,77 @@ export default {
 
 <template>
   <!--
-  <h1>Auto Nr. {{ this.$route.params.id }}</h1>
+  <h1>Auto Nr. {{ this.carID }}</h1>
   <pre>{{ car }}</pre>
--->
-  <main>
+  -->
+  <main v-if="car">
     <div class="single-car">
       <img
         :src="car.img_source"
-        :alt="'Auto Nr. ' + car.id"
-        :title="'Auto Nr. ' + car.id"
+        :alt="
+          car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
+        "
+        :title="
+          car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
+        "
       />
       <div class="car-details">
-        <h1>Marke - {{ car.type_id }}</h1>
-        <p><span>Typ</span> <span>Kategorie</span></p>
+        <h1>
+          {{ car.car_types.brands.brand_name }}
+          {{ car.car_types.car_type_name }}
+        </h1>
         <p>
-          <span>User</span> <span>000{{ car.user_id }}</span>
+          <span class="label user-label"></span>
+          <span>000{{ car.users.id }} - {{ car.users.username }}</span>
         </p>
         <p>
-          <span>Sitze</span> <span>{{ car.count_of_seats }}</span>
+          <span class="label">Typ</span>
+          <span class="car-type">{{ car.car_types.category }}</span>
         </p>
         <p>
-          <span>Baujahr</span> <span>{{ car.year_of_construction }}</span>
+          <span class="label">Sitze</span>
+          <span
+            class="seats-count"
+            :class="'seats-' + car.count_of_seats"
+          ></span>
         </p>
         <p>
-          <span>PS</span> <span>{{ car.kw }}</span>
+          <span class="label">Getriebe-Art</span> <span>{{ car.gear }}</span>
         </p>
         <p>
-          <span>max. Tempo</span> <span>{{ car.max_speed }} km/h</span>
-        </p>
-        <p>
-          <span>Kofferraum</span>
+          <span class="label">Kofferraum-Volumen</span>
           <span>{{ car.trunk_volume_in_liters }} l</span>
         </p>
         <p>
-          <span>Merkmale</span>
+          <span class="label">Kraftstoff</span>
+          <span>{{ car.fuel_type }},</span>
+        </p>
+        <p>
+          <span class="label">Verbrauch</span>
+          <span>{{ car.fuel_consume_per_km }} l/km</span>
+        </p>
+
+        <p>
+          <span class="label">PS</span> <span>{{ car.kw }}</span>
+        </p>
+        <p>
+          <span class="label">max. Tempo</span>
+          <span>{{ car.max_speed }} km/h</span>
+        </p>
+        <p>
+          <span class="label">Kilometerstand</span>
+          <span>{{ car.mileage }} km</span>
+        </p>
+        <p>
+          <span class="label">Baujahr</span>
+          <span>{{ car.year_of_construction }}</span>
+        </p>
+        <p>
+          <span class="label">Versicherung</span>
+          <span>{{ car.insurance_type }}</span>
+        </p>
+        <p>
+          <span class="label">Merkmale</span>
           <span>
             <ul>
               <li v-for="(feature, index) in car.cars_features" :key="index">
@@ -88,12 +109,12 @@ export default {
             </ul>
           </span>
         </p>
-        <!--
-        <p>{{ car.car_types }}</p>
-        -->
       </div>
     </div>
   </main>
+  <div v-else>
+    <p>Loading Car Nr. {{ this.carID }}</p>
+  </div>
 </template>
 
 <style scoped>
@@ -110,6 +131,7 @@ main {
 .single-car > img {
   width: 100%;
   border-radius: 1rem;
+  box-shadow: 3px 3px 10px var(--secondary-dark);
 }
 
 .single-car > .car-details > p {
@@ -122,8 +144,55 @@ main {
   margin-top: 0.5rem;
 }
 
-.single-car > .car-details > p > span:first-of-type {
+.single-car > .car-details > p > span.label {
   color: var(--primary-dark);
+}
+
+.single-car > .car-details > p > span.label.user-label::before {
+  content: "";
+  display: inline-block;
+  width: 18px;
+  aspect-ratio: 1;
+  background-color: var(--primary-dark);
+  border: 5px solid white;
+  border-radius: 100%;
+  outline: 2px solid var(--primary-mid);
+}
+
+.single-car > .car-details > p > span.car-type {
+  font-size: 1.1rem;
+  font-weight: 500;
+  color: var(--primary-dark);
+  background-color: var(--secondary-mid);
+  padding: 2px 5px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.single-car > .car-details > p > span.seats-count {
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: var(--primary-dark);
+}
+
+.single-car > .car-details > p > span.seats-count.seats-2::before {
+  content: "* *";
+}
+
+.single-car > .car-details > p > span.seats-count.seats-4::before {
+  content: "* * * *";
+}
+
+.single-car > .car-details > p > span.seats-count.seats-5::before {
+  content: "* * * * *";
+}
+
+.single-car > .car-details > p > span.seats-count.seats-6::before {
+  content: "* * * * * *";
+}
+
+.single-car > .car-details > p > span.seats-count.seats-8::before {
+  content: "* * * * * * * *";
 }
 
 .single-car > .car-details > p > span > ul {
