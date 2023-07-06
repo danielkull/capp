9b63f33e2e2af2d57227b9f199ae6387dd7cf8a1
Stefan Hiob <stefan.hiob@outlook.de>
2023-07-06 23:30:46 +0200

    feat: add button componente

---
+++ src/components/input-elements/Button.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <input type="button" value="Log In" class="Log-btn btn-font" id="LogIn-btn" />
+   -->
+  <input
+    type="button"
+    :value="`${sign_in}`"
+    class="Log-btn btn-font"
+    id="LogIn-btn"
+  />
+</template>
+
+<script>
+export default {
+  data: () => ({
+    log_in: "Log in",
+    sign_in: "Sign In",
+  }),
+};
+</script>
+
+<style scoped>
+.Log-btn {
+  all: unset;
+  cursor: pointer;
+  padding: calc(0.2rem + 1vh) calc(1.5rem + 1vw);
+  border-radius: calc(var(--s-brd-rad) + 0.5rem);
+  border: calc(var(--m-brd) + 1px) solid var(--primary-mid);
+  box-shadow: 0 0 0 0 var(--primary-mid) inset;
+  transition-duration: 0.5s, 0.2s;
+  transition-property: box-shadow, color;
+  background: var(--secondary-light);
+  outline: var(--m-brd) solid var(--secondary-mid);
+  outline-offset: clamp(0.3rem, 2vw, 0.5rem);
+}
+
+.btn-font {
+  color: var(--primary-mid);
+  font-size: clamp(1rem, 5vw, 1.2rem);
+  text-transform: uppercase;
+  font-weight: 900;
+  letter-spacing: 0.05em;
+}
+.Log-btn:focus {
+  outline-color: var(--primary-light);
+}
+.Log-btn:active {
+  transition: none;
+  background: var(--primary-dark);
+  color: var(--primary-light);
+}
+
+@media screen and (min-width: 900px) {
+  .Log-btn:hover,
+  .LogIn-card__btn:hover {
+    box-shadow: 20rem 0 0 0 var(--primary-mid) inset;
+    color: var(--secondary-light);
+    outline-color: var(--secondary-dark);
+  }
+  .Log-btn:active {
+    transition-property: box-shadow, color;
+    box-shadow: 20rem 0 0 0 var(--primary-dark) inset;
+    color: var(--primary-light);
+  }
+}
+</style>
