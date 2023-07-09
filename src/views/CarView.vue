9b3ea4fe2b0d9b9da28209a89e97fe4297eae6d2
kiraGit67 <kirsten@sais.de>
2023-07-09 18:08:43 +0200

    feat: beautifies responsive design of single CarView.vue

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -35,89 +35,92 @@ export default {
 <template>
   <main v-if="car">
     <div class="single-car">
-      <img
-        :src="car.img_source"
-        :alt="
-          car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
-        "
-        :title="
-          car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
-        "
-      />
+      <div class="img-wrapper">
+        <img
+          :src="car.img_source"
+          :alt="
+            car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
+          "
+          :title="
+            car.car_types.brands.brand_name + ' ' + car.car_types.car_type_name
+          "
+        />
+      </div>
       <div class="car-details">
         <h1>
           {{ car.car_types.brands.brand_name }}
           {{ car.car_types.car_type_name }}
         </h1>
-        <p>
-          <span class="label user-label"></span>
-          <span
-            >{{ car.users.firstname }} {{ car.users.lastname }}<br />{{
-              car.users.address
-            }}<br />{{ car.users.zipcode }} {{ car.users.city }}</span
-          >
-        </p>
-        <p>
-          <span class="label">Typ</span>
-          <span><IconType :carType="car.car_types.category" /></span>
-        </p>
-        <p>
-          <span class="label">Sitze</span>
-          <span>
-            <IconSeatsCount :countOfSeats="car.count_of_seats" />
-          </span>
-        </p>
-        <p>
-          <span class="label">Getriebe-Art</span> <span>{{ car.gear }}</span>
-        </p>
-        <p>
-          <span class="label">Kofferraum-Volumen</span>
-          <span>{{ car.trunk_volume_in_liters }} l </span>
-        </p>
-        <p>
-          <span class="label">Kofferraum-Größe</span>
-          <span
-            ><TrunkType :trunkVolumeLiters="car.trunk_volume_in_liters"
-          /></span>
-        </p>
-        <p>
-          <span class="label">Kraftstoff</span>
-          <span><fuelType :fuelType="car.fuel_type" /></span>
-        </p>
-        <p>
-          <span class="label">Verbrauch</span>
-          <span>{{ car.fuel_consume_per_km }} l/km</span>
-        </p>
+        <div class="col">
+          <p>
+            <span class="label user-label"></span>
+            <span
+              >{{ car.users.firstname }} {{ car.users.lastname }}<br />{{
+                car.users.address
+              }}<br />{{ car.users.zipcode }} {{ car.users.city }}</span
+            >
+          </p>
+          <p>
+            <span class="label">Typ</span>
+            <span><IconType :carType="car.car_types.category" /></span>
+          </p>
+          <p>
+            <span class="label">Sitze</span>
+            <span>
+              <IconSeatsCount :countOfSeats="car.count_of_seats" />
+            </span>
+          </p>
+          <p>
+            <span class="label">Getriebe</span> <span>{{ car.gear }}</span>
+          </p>
+          <p>
+            <span class="label">Kofferraum</span>
+            <span
+              ><TrunkType :trunkVolumeLiters="car.trunk_volume_in_liters" />
+              {{ car.trunk_volume_in_liters }} l</span
+            >
+          </p>
+        </div>
+        <div class="col">
+          <p>
+            <span class="label">Kraftstoff</span>
+            <span><fuelType :fuelType="car.fuel_type" /></span>
+          </p>
+          <p>
+            <span class="label">Verbrauch</span>
+            <span>{{ car.fuel_consume_per_km }} l/km</span>
+          </p>
 
-        <p>
-          <span class="label">PS</span> <span>{{ car.kw }}</span>
-        </p>
-        <p>
-          <span class="label">max. Tempo</span>
-          <span>{{ car.max_speed }} km/h</span>
-        </p>
-        <p>
-          <span class="label">Kilometerstand</span>
-          <span>{{ car.mileage }} km</span>
-        </p>
-        <p>
-          <span class="label">Baujahr</span>
-          <span>{{ car.year_of_construction }}</span>
-        </p>
-        <p>
-          <span class="label">Versicherung</span>
-          <span>{{ car.insurance_type }}</span>
-        </p>
-        <p>
-          <span class="label">Merkmale</span>
-          <span>
-            <ul>
-              <li v-for="(feature, index) in car.cars_features" :key="index">
-                {{ feature.features.feature_name }}
-              </li>
-            </ul>
-          </span>
-        </p>
+          <p>
+            <span class="label">PS</span> <span>{{ car.kw }}</span>
+          </p>
+          <p>
+            <span class="label">max. Tempo</span>
+            <span>{{ car.max_speed }} km/h</span>
+          </p>
+          <p>
+            <span class="label">Kilometerstand</span>
+            <span>{{ car.mileage }} km</span>
+          </p>
+          <p>
+            <span class="label">Baujahr</span>
+            <span>{{ car.year_of_construction }}</span>
+          </p>
+          <p>
+            <span class="label">Versicherung</span>
+            <span>{{ car.insurance_type }}</span>
+          </p>
+          <p>
+            <span class="label">Merkmale</span>
+            <span>
+              <ul>
+                <li v-for="(feature, index) in car.cars_features" :key="index">
+                  {{ feature.features.feature_name }}
+                </li>
+              </ul>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   </main>
@@ -134,30 +137,46 @@ main {
 .single-car {
   display: grid;
   grid-template-columns: 1fr;
-  gap: 1rem;
 }
 
-.single-car > img {
+.single-car > .img-wrapper,
+.single-car > .img-wrapper > img {
   width: 100%;
-  border-radius: 1rem;
-  box-shadow: 3px 3px 10px var(--secondary-dark);
+  aspect-ratio: 16 / 9;
+  object-fit: cover;
+  object-position: center center;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
 }
 
-.single-car > .car-details > p {
+.single-car > .car-details {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  padding-block: 1.25rem;
+  padding-inline: 1rem;
+  background-color: white;
+  border-radius: 1rem;
+  box-shadow: 0 0 10px var(--secondary-dark);
+  margin-top: -1.25rem;
+}
+
+.single-car > .car-details > .col > p {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  font-weight: 300;
+  font-weight: 400;
+  font-size: 1.15rem;
 }
 
-.single-car > .car-details > p + p {
-  margin-top: 0.5rem;
+.single-car > .car-details > .col > p + p {
+  margin-top: 0.75rem;
 }
 
-.single-car > .car-details > p > span.label {
+.single-car > .car-details > .col > p > span.label {
   color: var(--primary-dark);
 }
 
-.single-car > .car-details > p > span.label.user-label::before {
+.single-car > .car-details > .col > p > span.label.user-label::before {
   content: "";
   display: inline-block;
   width: 18px;
@@ -168,7 +187,7 @@ main {
   outline: 2px solid var(--primary-mid);
 }
 
-.single-car > .car-details > p > span.car-type {
+.single-car > .car-details > .col > p > span.car-type {
   font-size: 1.1rem;
   font-weight: 500;
   color: var(--primary-dark);
@@ -178,37 +197,37 @@ main {
   text-align: center;
 }
 
-.single-car > .car-details > p > span.seats-count {
+.single-car > .car-details > .col > p > span.seats-count {
   font-size: 1.5rem;
   font-weight: 700;
   color: var(--primary-dark);
 }
 
-.single-car > .car-details > p > span.seats-count.seats-2::before {
+.single-car > .car-details > .col > p > span.seats-count.seats-2::before {
   content: "* *";
 }
 
-.single-car > .car-details > p > span.seats-count.seats-4::before {
+.single-car > .car-details > .col > p > span.seats-count.seats-4::before {
   content: "* * * *";
 }
 
-.single-car > .car-details > p > span.seats-count.seats-5::before {
+.single-car > .car-details > .col > p > span.seats-count.seats-5::before {
   content: "* * * * *";
 }
 
-.single-car > .car-details > p > span.seats-count.seats-6::before {
+.single-car > .car-details > .col > p > span.seats-count.seats-6::before {
   content: "* * * * * *";
 }
 
-.single-car > .car-details > p > span.seats-count.seats-8::before {
+.single-car > .car-details > .col > p > span.seats-count.seats-8::before {
   content: "* * * * * * * *";
 }
 
-.single-car > .car-details > p > span > ul {
+.single-car > .car-details > .col > p > span > ul {
   list-style-type: none;
 }
 
-.single-car > .car-details > p > span > ul > li::before {
+.single-car > .car-details > .col > p > span > ul > li::before {
   content: "+ ";
   font-weight: 500;
   color: var(--primary-mid);
@@ -221,12 +240,23 @@ h1 {
 }
 
 @media screen and (min-width: 768px) {
-  .single-car {
+  .single-car > .car-details {
     grid-template-columns: repeat(2, 1fr);
-    gap: 1.5rem;
   }
 
-  .single-car > .car-details > p {
+  .single-car > .car-details > h1 {
+    grid-column: span 2;
+  }
+}
+
+@media screen and (min-width: 992px) {
+  .single-car > .img-wrapper,
+  .single-car > .img-wrapper > img {
+    aspect-ratio: 400 / 215;
+    object-fit: cover;
+  }
+
+  .single-car > .car-details > .col > p {
     grid-template-columns: 2fr 3fr;
   }
 }
