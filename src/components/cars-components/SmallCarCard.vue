4446ba3730501cd55fe3493a8aa99f51cd43cf93
kiraGit67 <kirsten@sais.de>
2023-07-11 20:04:59 +0200

    adds props to src/components/cars-components/SmallCarCard.vue

--- src/components/cars-components/SmallCarCard.vue
+++ src/components/cars-components/SmallCarCard.vue
@@ -1,11 +1,13 @@
 <template>
   <article class="car-card__wrapper">
     <img
-      src="https://ais-cf.tvnow.de/tvnow/movie/5429467/1800x0/folge-vom-23042023.jpg"
-      alt="Das Auto des Users"
+      :src="imgSource"
+      :alt="brandName + ' ' + carTypeName"
+      :title="brandName + ' ' + carTypeName"
     />
 
     <section class="car-card__important-info">
+      <h2>{{ brandName }} {{ carTypeName }}</h2>
       <section class="car-card__basic-information">
         <span class="car-card__adress">
           <svg
@@ -20,207 +22,26 @@
               d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
             />
           </svg>
-          <p>01097</p>
+          <p>{{ carUserZipCode }}</p>
           <p>/</p>
-          <p>Dresden, Neustadt</p>
+          <p>{{ carUserCity }}</p>
         </span>
         <span class="car-card__seat-icon-wrapper">
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            viewBox="0, 0, 14.173, 19.843"
-          >
-            <g id='"_-Layer_4'>
-              <g>
-                <path
-                  d="M12.687,13.188 L12.944,13.202 C13.614,13.38 13.687,16.612 12.87,17.787 L12.401,18.391 L10.517,18.391 C9.666,18.407 8.699,18.459 7.859,18.391 L6.58,18.391 L6.58,18.391 L6.314,18.391 C5.474,18.459 4.507,18.407 3.656,18.391 L1.772,18.391 L1.303,17.787 C0.486,16.612 0.56,13.38 1.229,13.202 L1.486,13.188 C2.327,13.441 2.68,14.262 2.961,15.024 C4.246,14.478 5.716,14.522 7.087,14.478 C8.457,14.522 9.928,14.478 11.213,15.024 C11.493,14.262 11.846,13.441 12.687,13.188 z"
-                />
-                <path
-                  d="M10.458,5.226 C10.969,5.226 11.382,5.644 11.382,6.16 L11.662,12.989 C10.916,13.822 11.16,13.386 10.834,14.236 C9.669,13.786 8.325,13.807 7.087,13.749 C5.834,13.774 4.542,13.832 3.35,14.264 C3.013,13.386 3.264,13.829 2.511,12.989 L2.791,6.16 C2.791,5.644 3.205,5.226 3.715,5.226 L10.458,5.226 z"
-                />
-                <path
-                  d="M4.141,4.451 L4.079,4.1 L4.079,2.562 C4.079,1.93 4.528,1.417 5.081,1.417 L9.093,1.417 C9.646,1.417 10.094,1.93 10.094,2.562 L10.094,4.1 L10.032,4.451 L4.141,4.451 z"
-                />
-                <path
-                  d="M10.594,5.726 C10.616,7.243 4.149,12.292 4.64,14.546"
-                  fill-opacity="0"
-                  stroke-width="1"
-                />
-              </g>
-            </g>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
-            fill="currentColor"
-            class="bi bi-plus plus"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-            />
-          </svg>
+          <SeatIconsFrame :countOfSeats="seatsCount" />
         </span>
       </section>
       <section class="car-card__icon-wrapper" style="--svg-width: 2.4rem">
-        <IconType :carType="'Kleinwagen'" class="icon"></IconType>
-        <FuelType :fuelType="'Gas'" class="icon"></FuelType>
-        <TrunkType :trunkType="'S'" class="icon"></TrunkType>
+        <IconType :carType="carTypeCategory" class="icon"></IconType>
+        <fuelType :fuelType="fuelTypeName" class="icon"></fuelType>
+        <TrunkType :trunkVolumeLiters="trunkVolume" class="icon"></TrunkType>
         <DifferentType :differentType="'Isofix'" class="icon"></DifferentType>
         <DifferentType :differentType="'Nicht-Raucher'" class="icon">
         </DifferentType>
         <DifferentType :differentType="'Tiere'" class="icon"> </DifferentType>
+        <!---->
+      </section>
+      <section class="car-card__router-link">
+        <slot></slot>
       </section>
     </section>
   </article>
@@ -228,17 +49,19 @@
 
 <script>
 import IconType from "@/components/icon-type/IconType.vue";
-import FuelType from "@/components/icon-type/fuelType.vue";
+import fuelType from "@/components/icon-type/fuelType.vue";
 import TrunkType from "@/components/icon-type/TrunkType.vue";
 import DifferentType from "@/components/icon-type/DifferentTypes.vue";
+import SeatIconsFrame from "@/components/icon-type/SeatIcons.vue";
 
 export default {
   name: "SmallCarCard",
   components: {
     IconType,
-    FuelType,
+    fuelType,
     TrunkType,
     DifferentType,
+    SeatIconsFrame,
   },
   props: {
     imgSource: {
@@ -250,6 +73,14 @@ export default {
     },
     carTypeName: {
       type: String,
+      required: true,
+    },
+    fuelTypeName: {
+      type: String,
+      required: true,
+    },
+    trunkVolume: {
+      type: Number,
       required: true,
     },
     seatsCount: {
@@ -277,9 +108,15 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  text-align: center;
+  font-size: 1.25rem;
+  color: var(--primary-dark);
+  margin-bottom: 0.75rem;
+}
 .car-card__wrapper {
   width: 100%;
-  height: 18rem;
+  height: 27.5rem;
   position: relative;
   margin-bottom: 1rem;
   border-radius: 1rem;
@@ -291,15 +128,23 @@ export default {
 img {
   border-radius: 1rem 1rem 0 0;
   width: 100%;
+  aspect-ratio: 16 / 9;
+  object-fit: cover;
+  object-position: center center;
   filter: grayscale(var(--filter-img));
 }
 
 .car-card__basic-information {
   width: 95%;
   margin-inline: auto;
-
   display: flex;
 }
+
+.car-card__router-link {
+  width: 95%;
+  margin-inline: auto;
+}
+
 .car-card__important-info {
   position: absolute;
   display: flex;
@@ -341,7 +186,7 @@ img {
 }
 .car-card__icon-wrapper {
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   gap: 0.2rem;
   padding-left: -0.3rem;
