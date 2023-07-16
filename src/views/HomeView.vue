b47d42439ee5039a796fdb507697cdb3de82c00a
kiraGit67 <kirsten@sais.de>
2023-07-16 20:26:22 +0200

    imports UserQuestionMenue component into HomeView.vue to test functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h1>Test von Home View</h1>
+    <user-question-menue />
+    <p>&nbsp;</p>
+    <hr />
+    <p>&nbsp;</p>
     <filter-drop-down />
   </div>
 </template>
 
 <script>
 import FilterDropDown from "../components/FilterDropDown.vue";
+import UserQuestionMenue from "../components/UserQuestionMenue.vue";
 export default {
-  components: { FilterDropDown },
+  components: { FilterDropDown, UserQuestionMenue },
 };
 </script>
 
