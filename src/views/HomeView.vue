f16c4ca0e34865a2b035df548429108d722be292
kiraGit67 <kirsten@sais.de>
2023-07-17 22:05:58 +0200

    removes UserQuestionMenue component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
     <h1>Test von Home View</h1>
-    <user-question-menue />
-    <p>&nbsp;</p>
-    <hr />
-    <p>&nbsp;</p>
     <filter-drop-down />
   </div>
 </template>
 
 <script>
 import FilterDropDown from "../components/FilterDropDown.vue";
-import UserQuestionMenue from "../components/UserQuestionMenue.vue";
 export default {
-  components: { FilterDropDown, UserQuestionMenue },
+  components: { FilterDropDown },
 };
 </script>
 
